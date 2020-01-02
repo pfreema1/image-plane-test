4b01d04bcf2f56631a6666caf56b86e8d4cdf404
Paul <pfreema1@gmail.com>
2020-01-01 20:58:29 -0500

    trippy

--- src/scripts/TextureCube.js
+++ src/scripts/TextureCube.js
@@ -3,6 +3,7 @@ import imageTextureCubeFrag from '../shaders/imageTextureCube.frag';
 import imageTextureCubeVert from '../shaders/imageTextureCube.vert';
 import TweenMax from 'TweenMax';
 import glslify from 'glslify';
+import { Float32BufferAttribute } from 'three';
 
 export default class TextureCube {
     constructor(webGLView, width, height, imageTexture, gridDim, id, zVal, pD) {
@@ -13,6 +14,7 @@ export default class TextureCube {
         const columns = gridDim.height;
 
         const geo = new THREE.BoxBufferGeometry(width, height, 1);
+        this.updateUv(geo);
         // const geo = new THREE.PlaneBufferGeometry(width, height, 32);
         const mat = new THREE.ShaderMaterial({
             uniforms: {
@@ -49,5 +51,42 @@ export default class TextureCube {
             delay: 0.5 * id.j + id.i * 0.2,
             y: Math.PI * 0.2
         });
+    }
+
+    updateUv(geo) {
+        const uvs = new Float32Array([
+            // front
+            0, 1,
+            1, 1,
+            0, 0,
+            1, 0,
+            // right
+            0, 1,
+            1, 1,
+            0, 0,
+            1, 0,
+            // back
+            0, 1,
+            1, 1,
+            0, 0,
+            1, 0,
+            // left
+            0, 1,
+            1, 1,
+            0, 0,
+            1, 0,
+            // top (this seems to be the front facing face!)
+            0, 2.5,
+            2.5, 2.5,
+            0, 0,
+            2.5, 0,
+            // bottom
+            0, 1,
+            1, 1,
+            0, 0,
+            1, 0,
+        ]);
+
+        geo.addAttribute('uv', new THREE.BufferAttribute(uvs, 2));
     }
 }
